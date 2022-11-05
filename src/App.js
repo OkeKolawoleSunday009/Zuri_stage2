e6d361cd4b0323a904ffe8f53a566145ae5005ce
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-11-05 23:26:31 +0100

    page reset

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Section from "./Zuri001/Section.js";
-import Footer from "./Zuri001/Footer.js";
-import SectionIcon from './Zuri001/SectionIcon';
+import Section from "./zuri001/Section.js";
+import Footer from "./zuri001/Footer.js";
+// import SectionIcon from './zuri001/SectionIcon';
 import  { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-import Contact from './Zuri001/Pages/Contact';
+import Contact from './zuri001/Pages/Contact';
 import {Link} from 'react-router-dom'
-import Section_comp from './Zuri001/Section_comp';
+import Section_comp from './zuri001/Section_comp';
 
 // import profile from "./Image/profile.jpeg"
 
