56b243004605cd50acd6feb3c4b39f84dacc537b
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2022-11-06 20:51:43 +0100

    align

--- src/Zuri001/Pages/Contact.js
+++ src/Zuri001/Pages/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     <div className='Flask'>
         <div className='sub__flask'>
            <div className="title"><h1>Contact Me</h1></div>
-            <div><p>Hi Dear contact me about anything you have in mind</p></div>
+            <div><p>Hi there contact me about anything you have in mind</p></div>
 
             <form action='post'>
 
@@ -15,7 +15,7 @@ export default function Contact() {
                 <label for="firstname">
                     First Name
                 </label>
-                <input type="text" id='firstname' className='inputtext' required/>
+                <input type="text" id='firstname' className='inputtext' placeholder='Enter your first name' required/>
                 </div>
 
                 <div className='name'>
@@ -23,7 +23,7 @@ export default function Contact() {
                 <label for="lastname">
                     Last Name
                 </label>
-                <input type="text" id='lastname' className='inputtext' required/>
+                <input type="text" id='lastname' className='inputtext' placeholder='Enter your last name' required/>
 
 
                 </div>
@@ -34,19 +34,19 @@ export default function Contact() {
                 <label for="email">
                     Email
                 </label>
-                <input type="text" id='email' className='inputtext2' required/>
+                <input type="text" id='email' className='inputtext2' placeholder='yourname@gmail.com' required/>
 
                 <label for="message">
                     Message
                 </label>
-                <input type="text-area" id='message' className='inputtext3' required/>
+                <input type="text-area" id='message' className='inputtext3' placeholder="Send me a message and i'll reply you as soon as possible" required/>
                 
                 <div className='check'>
                     
               
-                <input type="radio" id='check' required />
-                <p>
-                    You agree to your data the name, who may contact you
+                <input type="checkbox" id='check' required />
+                <p className='check_paragraph'>
+                    You agree to your data to the name, who may contact you.
                 </p>
                 
                 </div>
